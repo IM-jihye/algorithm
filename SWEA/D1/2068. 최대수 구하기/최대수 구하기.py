605c277d879a430bc613290584704ee826974142
임지혜 <121530997+IM-jihye@users.noreply.github.com>
2023-01-25 14:29:51 +0900

    [D1] Title: 최대수 구하기, Time: 88 ms, Memory: 43,528 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.py
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.py
@@ -1,5 +1,5 @@
-T = int(input())
-for t in range(1, T+1):
-     numbers = list(map(int,input().split()))
-     numbers = max(numbers)
-     print(f"#{t} {numbers}")
+t = int(input())
+for i in range(1,t+1):
+    numbers = list(map(int,input().split()))
+    num = max(numbers)
+    print(f'#{i} {num}')
